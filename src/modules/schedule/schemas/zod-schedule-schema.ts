a3c119807b244fbeb2baa91ada1c135472782a71
Felipe Lacerda <felipelacerda500@gmail.com>
2024-07-14 15:45:00 -0300

    feat: add id field to scheduleBodySchema

--- src/modules/schedule/schemas/zod-schedule-schema.ts
+++ src/modules/schedule/schemas/zod-schedule-schema.ts
@@ -1,2 +1,9 @@
 import { factoryDayHelper } from '@/shared/factories';
 import { z } from 'zod';
+
+export const scheduleBodySchema = z.object({
+  id: z
+    .string({ message: 'ID deve ser uma string.' })
+    .uuid({ message: 'ID deve ser um UUID válido.' })
+    .optional(),
+});
