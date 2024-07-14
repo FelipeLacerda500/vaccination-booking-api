4ba29a0e1219691a54b051a4f6ce0f533d536158
Felipe Lacerda <felipelacerda500@gmail.com>
2024-07-14 17:02:49 -0300

    feat: add scheduleDate field to scheduleBodySchema

--- src/modules/schedule/schemas/zod-schedule-schema.ts
+++ src/modules/schedule/schemas/zod-schedule-schema.ts
@@ -4,6 +4,8 @@ import { factoryDateHelper } from '@/shared/factories';
 import { z } from 'zod';
 
 const dateHelper = factoryDateHelper();
+const START_RANGE_HOUR = 8;
+const END_RANGE_HOUR = 18;
 
 export const scheduleBodySchema = z.object({
   id: z
@@ -28,6 +30,36 @@ export const scheduleBodySchema = z.object({
       {
         message:
           'A data de nascimento do paciente não pode ser uma data futura.',
+      },
+    )
+    .transform((date) => dateHelper.convertToUtc(date))
+    .optional(),
+  scheduleDate: z
+    .string({ message: 'A data do agendamento deve ser uma string.' })
+    .datetime({
+      message: 'A data do agendamento deve ser uma data válida.',
+    })
+    .refine(
+      (date) => {
+        const providedScheduleDate = new Date(date);
+
+        if (
+          !dateHelper.hasZeroMinutesAndSeconds(providedScheduleDate) ||
+          !dateHelper.isFutureDate(providedScheduleDate) ||
+          !dateHelper.isTimeInRange(
+            providedScheduleDate,
+            START_RANGE_HOUR,
+            END_RANGE_HOUR,
+          )
+        ) {
+          return false;
+        }
+
+        return true;
+      },
+      {
+        message:
+          'Sobre o agendamento, a data deve ser futura, o horário deve ser em ponto e estar entre 8h às 18h.',
       },
     )
     .transform((date) => dateHelper.convertToUtc(date))
