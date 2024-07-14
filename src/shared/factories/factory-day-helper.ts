b55bb98731da391142c04d77f46ae034f9d73925
Felipe Lacerda <felipelacerda500@gmail.com>
2024-07-14 14:38:23 -0300

    feat: add factoryDayHelper

---
+++ src/shared/factories/factory-day-helper.ts
@@ -0,0 +1,5 @@
+import { DayjsHelper } from '../helpers';
+
+export function factoryDayHelper() {
+  return new DayjsHelper();
+}
--- src/shared/factories/index.ts
+++ src/shared/factories/index.ts
@@ -1,1 +1,2 @@
 export * from './factory-env-var-validator';
+export * from './factory-day-helper';
