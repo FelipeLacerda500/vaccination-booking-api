0f0099539a052932769e722e3e1bc3518a726da3
Felipe Lacerda <felipelacerda500@gmail.com>
2024-07-15 09:55:18 -0300

    feat: add create method to InMemoryScheduleRepository

---
+++ src/modules/schedule/repositories/monostate/in-memory-schedule-repository.ts
@@ -0,0 +1,27 @@
+/* eslint-disable require-await, camelcase */
+
+import { randomUUID } from 'node:crypto';
+import { Schedule } from '../../models';
+import { IScheduleRepository } from '../../protocols';
+
+export class InMemoryScheduleRepository implements IScheduleRepository {
+  private static _schedules: Schedule[] = [];
+
+  public async create(data: Schedule) {
+    const schedule = {
+      id: randomUUID(),
+      patient_name: data.patient_name,
+      patient_birth_date: data.patient_birth_date,
+      schedule_date: data.schedule_date,
+      realized: false,
+    };
+
+    InMemoryScheduleRepository._schedules.push(schedule);
+
+    InMemoryScheduleRepository._schedules.sort(
+      (a, b) => a.schedule_date.getTime() - b.schedule_date.getTime(),
+    );
+
+    return schedule;
+  }
+}
