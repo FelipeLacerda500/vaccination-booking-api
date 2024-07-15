31576e7b5702e99b657e6628189972a1122717d1
Felipe Lacerda <felipelacerda500@gmail.com>
2024-07-15 17:28:31 -0300

    feat: add server routes

---
+++ src/server/routes/routes.ts
@@ -0,0 +1,16 @@
+/* eslint-disable require-await, arrow-body-style */
+
+import { FastifyInstance } from 'fastify';
+import {
+  createScheduleController,
+  listSchedulesController,
+  confirmScheduleController,
+  fetchSchedulesController,
+} from '@/modules/schedule/controllers';
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post('/', createScheduleController);
+  app.get('/schedules', listSchedulesController);
+  app.get('/schedules/search', fetchSchedulesController);
+  app.patch('/schedules/confirm/:id', confirmScheduleController);
+}
